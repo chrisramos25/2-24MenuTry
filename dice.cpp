7f26af08d4b5eda1f4f9898ce053ea119cc1b703
chrisramos25 <133292052+chrisramos25@users.noreply.github.com>
2026-02-25 00:12:12 -0600

    2/25 12:12

---
+++ dice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void playDicegame () {
+
+    //initizlize the word
+    std::string word = "LoneStar";
+    std::string guessedWord(word.length(), '_'); //finds length of word and outputs underscores for each letter
+    
+
+
+
+}
